e4786db690795adbb533ce9eb0716c03e49bfdf9
sukesh515 <sukesh515@yahoo.co.in>
2016-07-18 15:14:52 +0100

    ALl Project Commiting

---
+++ BritAirways/src/TestCases/FirstPage.java
@@ -0,0 +1,92 @@
+package TestCases;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import Utils.BrowserSelection;
+
+
+
+@Test
+public class FirstPage  extends BrowserSelection{
+
+	
+public void LoginBrowser() throws IOException, InterruptedException{
+		
+		BrowserSelect();
+		
+		driver.findElement(By.linkText("Continue")).click();
+		
+		Select dropdown = new Select(driver.findElement(By.xpath(".//*[@id='from']")));
+		
+		dropdown.selectByVisibleText("Belfast");
+		
+		driver.findElement(By.xpath(".//*[@id='planTripFlightDestination']")).sendKeys("Houston, George Bush Intercont (TX) (IAH), USA");
+		
+		driver.findElement(By.xpath(".//*[@id='depDate']")).click();
+		driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).click();
+		driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).click();
+	
+		
+		WebElement datepicker = driver.findElement(By.xpath(".//*[@id='ui-datepicker-div']"));  
+		// List<WebElement> rows=datepicker.findElements(By.tagName("tr"));
+		List<WebElement> columns=datepicker.findElements(By.tagName("td"));
+		
+		// System.out.println(columns);
+		
+		for (WebElement cell: columns){  
+			//Select 20th Date   
+			if (cell.getText().equals("22")){  
+			cell.findElement(By.linkText("22")).click();  
+			break;  
+			 }  
+			}   
+		
+		
+		driver.findElement(By.xpath(".//*[@id='retDate']")).click();
+		
+        Thread.sleep(300L); 
+		
+		driver.findElement(By.xpath("//span[@class='ui-icon ui-icon-circle-triangle-e']")).click();	
+		
+		WebElement datepickerre = driver.findElement(By.xpath(".//*[@id='ui-datepicker-div']"));  
+		// List<WebElement> rows=datepicker.findElements(By.tagName("tr"));
+		List<WebElement> columnre=datepickerre.findElements(By.tagName("td"));
+		
+		
+		
+		
+		for (WebElement cell: columnre){  
+			//Select 20th Date   
+			if (cell.getText().equals("24")){  
+			cell.findElement(By.linkText("24")).click();  
+			break;  
+			 }  
+			}  
+		
+		
+		Select dropdownAdults = new Select(driver.findElement(By.xpath(".//*[@id='ad']")));
+		
+		dropdownAdults.selectByVisibleText("2");
+		
+       Select dropdownchild = new Select(driver.findElement(By.xpath(".//*[@id='ch']")));
+		
+		dropdownchild.selectByVisibleText("1");
+		
+		
+		driver.findElement(By.xpath(".//*[@id='flightSearchButton']")).click();
+		
+		
+	}
+	
+
+	
+	
+}
---
+++ BritAirways/src/Utils/BrowserSelection.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package Utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+/**
+ * @author sukeshswathi This is to select browser to run
+ *
+ */
+public class BrowserSelection {
+
+	public static WebDriver driver;
+	
+	
+	/*public BrowserSelection (WebDriver driver){
+		
+		
+		this.driver = driver;
+	}*/
+	
+	public void BrowserSelect() throws IOException {
+		
+
+		Properties prop = new Properties();
+		
+		FileInputStream fil = new FileInputStream(System.getProperty("user.dir")+ "\\src\\datadriven.properties");
+		
+		//FileInputStream fil = new FileInputStream("datadriven.properties");
+				
+		prop.load(fil);
+
+		if(prop.getProperty("browser").equals("firefox"))
+
+		{
+
+			// System.setProperty("webdriver.firefox.driver", "C:\\firefox\\FirefoxSetup24.2.0esr.exe");	
+			
+			
+		driver =new FirefoxDriver();
+
+		}
+
+		else if (prop.getProperty("browser").equals("chrome"))
+
+		{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sukeshswathi\\Documents\\TarunSelenium\\chromedriver_win32\\chromedriver.exe");
+		driver =new ChromeDriver();
+
+		}
+		else {
+			
+			driver = new InternetExplorerDriver();
+		}
+		
+		
+		driver.get(prop.getProperty("url"));
+		
+	}
+	
+	
+	
+}
