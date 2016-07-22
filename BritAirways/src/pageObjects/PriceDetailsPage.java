c345c16fe2a6bde100a347dba70e5048e69fdae8
sukesh515 <sukesh515@yahoo.co.in>
2016-07-22 11:41:00 +0100

    Price Detail Page added Objects

--- BritAirways/src/pageObjects/PriceDetailsPage.java
+++ BritAirways/src/pageObjects/PriceDetailsPage.java
@@ -1,5 +1,52 @@
 package pageObjects;
 
-public class PriceDetailsPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-}
+public class PriceDetailsPage {
+	
+	WebDriver driver;
+	
+	public PriceDetailsPage (WebDriver driver){
+		
+		this.driver = driver;
+		
+	}
+		By outb = By.id("outbound-M-4");
+		
+		By inb = By.id("inbound-M-1");
+		
+		By contin = By.xpath(".//*[@id='continue']");
+		
+		
+		public WebElement Outbound() {
+			
+			
+			return driver.findElement(outb);
+			
+		}
+		
+		
+           public WebElement Inbound() {
+			
+			
+			return driver.findElement(inb);
+			
+		}
+		
+		
+           public WebElement PriceDisplay() {
+   			
+   			
+   			return driver.findElement(contin);
+   			
+   		}
+   		
+		
+		
+		
+		
+	}
+
+
